2c15cdb72c50dbbc6d11ff37ec433db0c0bd1386
QueerPassenger <allanmvalooran@gmail.com>
2020-08-18 19:22:33 +0530

    WithDocsAndIcons

--- app.config.js
+++ app.config.js
@@ -1,13 +1,14 @@
 module.exports = function ({ config, mode }) {
-    const { APP_NAME, VERSION, ANDROID_PACKAGE, GOOGLE_API_KEY, GOOGLE_CERTIFICATE_HASH, API_URL, ANDROID_CLIENT_ID, ANDROID_STANDALONE_APP_CLIENT_ID } = process.env;
+    const { APP_NAME, SLUG, VERSION, ANDROID_PACKAGE, GOOGLE_API_KEY, GOOGLE_CERTIFICATE_HASH, API_URL, ANDROID_CLIENT_ID, ANDROID_STANDALONE_APP_CLIENT_ID } = process.env;
     const expoConfig = {
         "name": APP_NAME,
-        "slug": APP_NAME,
+        "slug": SLUG,
         "version": VERSION,
         "orientation": "portrait",
         "updates": {
             "fallbackToCacheTimeout": 0
         },
+        "icon": "./assets/images/app-icon.png",
         "assetBundlePatterns": [
             "**/*"
         ],
