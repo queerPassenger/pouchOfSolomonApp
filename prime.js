aa93da1f43a3d2d183b46c4c219be5931204ed35
QueerPassenger <allanmvalooran@gmail.com>
2020-08-18 16:40:30 +0530

    ExecTOSpawn

--- prime.js
+++ prime.js
@@ -1,6 +1,15 @@
 require('dotenv').config();
-const { exec } = require('child_process');
+const { exec, spawn } = require('child_process');
+
+/* exec
 const execCommand = process.argv.slice(2).join(' ');
 const execProcess = exec(execCommand);
 execProcess.stdout.pipe(process.stdout);
 execProcess.stderr.pipe(process.stderr);
+execProcess.stdin.pipe(process.stdin);
+*/
+const rootCommand = process.argv[2];
+const argumentCommands = process.argv.slice(3);
+spawn(rootCommand, argumentCommands,
+    { stdio: [process.stdin, process.stdout, process.stderr] });
+
